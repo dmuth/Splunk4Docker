16aea15ee91f9c7fd77a24f97c455b5a00154f9e
Douglas Muth <dmuth@dmuth.org>
2015-02-01 00:41:21 -0500

    Added symlinks for the logfile directory

--- splunk/runtime.sh
+++ splunk/runtime.sh
@@ -9,12 +9,27 @@
 set -e
 
 #
+# Where to log our command output.
+#
+LOG="/splunk-logs/output.txt"
+
+
+#
 # Install Splunk
 #
 echo "# "
 echo "# Installing Splunk..."
 echo "# "
-dpkg -i /data-install/splunk.deb
+dpkg -i /data-install/splunk.deb 2>&1 | tee -a ${LOG}
+
+
+echo "# "
+echo "# Setting up symlinks for Splunk logs to /splunk-logs/, which is exported from Docker"
+echo "# "
+/var/splunk/bin/splunk --accept-license status 2>&1 | tee -a ${LOG}
+rm -rf /opt/splunk/var/log
+ln -s /splunk-logs/ /opt/splunk/var/log
+
 
 #
 # Run Splunk in the foreground
@@ -22,7 +37,6 @@ dpkg -i /data-install/splunk.deb
 echo "# "
 echo "# Running Splunk in the foreground..."
 echo "# "
-/var/splunk/bin/splunk --accept-license start --nodaemon
-
+/var/splunk/bin/splunk --accept-license start --nodaemon 2>&1 | tee -a ${LOG}
 
 

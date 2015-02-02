2bdae82f76d9c37b0b6c23dfecec64161c8aa2ee
Douglas Muth <dmuth@dmuth.org>
2015-02-01 19:08:34 -0500

    Added parameter support and the current directory for development purposes.

--- splunk/go.sh
+++ splunk/go.sh
@@ -9,15 +9,42 @@
 #
 set -e 
 
+#
+# Check our arguments
+#
+if test "$1" == "-h"
+then
+	echo "Syntax: $0 <command to run in this image>"
+	echo ""
+	echo "To make this image be interactive, type '$0 bash'"
+	echo ""
+	exit 1
+fi
+
+
 echo "# "
 echo "# Building Docker image..."
 echo "# "
 ./build.sh
 
 
+VOLUMES=""
+#
+# Make our logs visible to the outside world
+#
+VOLUMES="${VOLUMES} -v /home/core/vagrant/splunk/logs:/splunk-logs "
+#
+# Put the current directory in as /data-devel for development purposes
+#
+VOLUMES="${VOLUMES} -v /home/core/vagrant/splunk/:/data-devel "
+
+
 echo "# "
 echo "# Running Docker image..."
 echo "# "
-docker run -it -p 8000:8000 -v /home/core/vagrant/splunk/logs:/splunk-logs dmuth/splunk 
+docker run -it \
+	-p 8000:8000 \
+	${VOLUMES} \
+	dmuth/splunk $@
 
 
